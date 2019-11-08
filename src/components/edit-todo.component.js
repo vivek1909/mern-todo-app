061bffee95733dada103667bebaafdbf446abdd7
Vivek Mittal <42748450+vivek1909@users.noreply.github.com>
2019-11-08 18:14:00 +0530

    joined frontend & backend

--- src/components/edit-todo.component.js
+++ src/components/edit-todo.component.js
@@ -1,6 +1,34 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 class EditTodo extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      todo_description: "",
+      todo_responsible: "",
+      todo_priority: "",
+      todo_completed: false
+    };
+  }
+
+  componentDidMount() {
+    axios
+      .get("http://localhost:4000/todos/" + this.props.match.params.id)
+      .then(response => {
+        this.setState({
+          todo_description: response.data.todo_description,
+          todo_responsible: response.data.todo_responsible,
+          todo_priority: response.data.todo_priority,
+          todo_completed: response.data.todo_completed
+        });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+
   render() {
     return (
       <div>
